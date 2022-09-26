19f62556463ece5446adba0bd435774cd8318d3a
hyenbin <sade.a@foxmail.com>
2022-09-27 01:15:42 +0800

    git test 1.0

---
+++ src/main/java/com/study/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.study.git;
+
+/**
+ * @Author hyenbin
+ * @Description
+ * @Date 2022/9/27 1:13
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
